045af4279dea25d257295209a3dc415e1a51fded
Tafarel Yan <tafarel.yan@gmail.com>
2025-06-13 20:09:27 -0300

    ✨ Include COR policy for local testing

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,10 @@ import json
 import sqlite3
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from typing import Any, Dict, List, Optional
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
 
 DATABASE_URL = "database.db"
 
@@ -45,6 +46,22 @@ app = FastAPI(
     lifespan=lifespan
 )
 
+origins = [
+    'http://localhost',
+    'http://localhost:8000',
+    'http://localhost:8080',
+    'http://127.0.0.1:5500',
+    'file://',
+    'null'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 class CardBase(BaseModel):
     title: str
